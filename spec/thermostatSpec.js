7fdf69ca7292d1856966a1f5dfdf83b78bfe2cff
Student <student@Makerss-MacBook-Pro-8.local>
2019-11-15 09:43:35 +0000

    Refactored and implemented changes - started jQuery shows temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,31 +1,83 @@
-// 'use strict';
+'use strict';
 
-describe('Thermostat', function() {
+describe('Thermostat', function(){
 
-  // var thermostat;
+  var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  it('starts with a default of 20 degrees', function(){
-    expect(thermostat.currentTemp()).toEqual(20);
-  });
+  describe('temperature', function(){
+    it('starts with a default of 20 degrees', function(){
+      expect(thermostat.currentTemp()).toEqual(20);
+    });
 
-  it('increases tempature when called', function() {
-    thermostat.increaseTemp()
-    expect(thermostat.currentTemp()).toEqual(21);
-  });
+    it('increases tempature when called', function(){
+      thermostat.increaseTemp()
+      expect(thermostat.currentTemp()).toEqual(21);
+    });
 
-  it('decreases temperature when called', function() {
-    thermostat.decreaseTemp()
-    expect(thermostat.currentTemp()).toEqual(19);
-  });
-
-  it('temperature cannot drop below 10 degrees', function() {
-    for (var i = 0; i < 11; i++) {
+    it('decreases temperature when called', function(){
       thermostat.decreaseTemp()
-    }
-    expect(thermostat.currentTemp()).toEqual(10);
+      expect(thermostat.currentTemp()).toEqual(19);
+    });
+
+    it('temperature cannot drop below 10 degrees', function(){
+      for (var i = 0; i < 11; i++) {
+        thermostat.decreaseTemp()
+      }
+      expect(thermostat.currentTemp()).toEqual(10);
+    });
+
+    it('reset temperature to 20 degrees with a reset', function(){
+      thermostat.increaseTemp();
+      thermostat.resetTemp();
+      expect(thermostat.currentTemp()).toEqual(20);
+    });
   });
+
+  describe('power save mode', function(){
+    it('Power save mode on has a maximum temperature of 25 degrees', function(){
+      thermostat.powerSavingModeOn()
+      for (var i = 0; i < 6; i++){
+        thermostat.increaseTemp();
+      }
+      expect(thermostat.currentTemp()).toEqual(25)
+    });
+
+    it('Power save mode off has a maximum temperature of 32 degrees', function(){
+      thermostat.powerSavingModeOff()
+      for (var i = 0; i < 13; i++){
+        thermostat.increaseTemp();
+      }
+      expect(thermostat.currentTemp()).toEqual(32)
+    });
+
+    it('Power saving mode is on by default', function(){
+      expect(thermostat.powerSavingModeOn()).toEqual(true);
+    });
+  });
+
+  describe('energy usage', function(){
+    it('returns low usage when < 18', function(){
+      for (var i = 0; i < 3; i++){
+        thermostat.decreaseTemp();
+      }
+      expect(thermostat.energyUsage()).toEqual('low-usage')
+    });
+
+    it('returns medium usage when < 25', function(){
+      expect(thermostat.energyUsage()).toEqual('medium-usage')
+    });
+
+    it('returns hi usage when > 25', function(){
+      thermostat.powerSavingModeOff();
+      for (var i = 0; i < 6; i++){
+        thermostat.increaseTemp();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage')
+    });
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,23 +1,56 @@
-// 'use strict';
+'use strict';
 
-function Thermostat() {
+function Thermostat(){
   this.temperature = 20;
   this.MIN_TEMP = 10;
+  this.maxTemp = 25;
 };
-Thermostat.prototype.currentTemp = function() {
+
+Thermostat.prototype.currentTemp = function(){
   return this.temperature
 };
-Thermostat.prototype.isMinTemp = function() {
+
+Thermostat.prototype.isMinTemp = function(){
   return this.temperature === this.MIN_TEMP;
 };
-Thermostat.prototype.increaseTemp = function() {
+Thermostat.prototype.isMaxTemp = function () {
+  return this.temperature === this.maxTemp;
+};
+
+Thermostat.prototype.increaseTemp = function(){
+  if (this.isMaxTemp()) {
+    return;
+  }
   this.temperature += 1;
   return this.temperature
 };
-Thermostat.prototype.decreaseTemp = function() {
+Thermostat.prototype.decreaseTemp = function(){
   if (this.isMinTemp()) {
     return;
   }
   this.temperature -= 1;
   return this.temperature
+};
+Thermostat.prototype.resetTemp = function () {
+  this.temperature = 20;
+  return this.temperature
+};
+
+Thermostat.prototype.powerSavingModeOn = function (){
+  this.maxTemp = 25;
+  return true
+};
+Thermostat.prototype.powerSavingModeOff = function (){
+  this.maxTemp = 32;
+  return false
+};
+
+Thermostat.prototype.energyUsage = function () {
+  if (this.temperature < 18) {
+    return 'low-usage'
+  } else if (this.temperature < 25 && this.temperature > 18) {
+    return 'medium-usage'
+  } else if (this.temperature > 25) {
+    return 'high-usage'
+  }
 };
